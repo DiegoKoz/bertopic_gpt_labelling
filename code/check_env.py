bc633caf51491a46997abed4082f60c99fa2b79a
Diego Kozlowski <diegokoz92@gmail.com>
2024-08-13 11:17:30 -0400

    remove temp file

--- code/check_env.py
+++
@@ -1,8 +0,0 @@
-import os
-print("Current Working Directory:", os.getcwd())
-
-file_path = 'data/biology_abstracts.csv'
-if os.path.exists(file_path):
-    print("File found")
-else:
-    print("File not found")
